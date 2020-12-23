ed3397bc249f538d48afd76b4a4b3d500488ab9d
VivienECE <vivien.detournay@edu.ece.fr>
2020-12-23 23:47:28 +0100

    fix docker-compose

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,7 +7,7 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.BACKEND_PORT || 3000
 
 const client = require('./dbClient')
 client.on("error", (err) => {
